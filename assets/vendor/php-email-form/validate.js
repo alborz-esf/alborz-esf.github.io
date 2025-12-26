ca79248473a196bcb2bd5cd66d4d1cb3f5aff73e
Alborz Esfandyari <alborz.esf@gmail.com>
2025-12-26 16:58:31 +0330

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,7 +6,7 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('.1php-email-form');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
@@ -83,3 +83,4 @@
   }
 
 })();
+
