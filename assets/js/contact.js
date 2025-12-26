ea16cbf6683bc2d47f9c4d18909c73f5022de299
Alborz Esfandyari <alborz.esf@gmail.com>
2025-12-26 16:53:42 +0330

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -21,12 +21,12 @@ document.addEventListener("DOMContentLoaded", function () {
           if (Object.hasOwn(data, 'errors')) {
             status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
           } else {
-            status.innerHTML = "Oops! There was a problem submitting your form"
+            status.innerHTML = "Oops! There was a problem submitting your form."
           }
         })
       }
     }).catch(error => {
-      status.innerHTML = "Oops! There was a problem submitting your form"
+      status.innerHTML = "Oops! There was a problem submitting your form."
     });
   }
   form.addEventListener("submit", handleSubmit)
